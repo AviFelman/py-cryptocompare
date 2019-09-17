81c9b88fb0394347d9640ba7bee3890efb9d20e3
Avi Felman <avifelman@Avis-MacBook-Pro.local>
2019-09-17 15:48:34 -0700

    added volatility

--- CryptoCompareREST.py
+++ CryptoCompareREST.py
@@ -4,7 +4,8 @@ import numpy as np
 import requests
 from datetime import datetime
 from pandas.io.json import json_normalize
-
+from pandas.api.types import is_string_dtype
+from pandas.api.types import is_numeric_dtype
 
 
 ## Create the class with all the pertinent paramaters
@@ -18,7 +19,6 @@ class cryptocompareAPI(object):
     def request(self, action, params):
         headers = {'authorization': 'Apikey %s' % (self.key)}
         response = requests.get(self.url + action, headers=headers, params=params)
-        print(response.url)
         # Raise exceptions if something goes wrong with the API or a specific call.
         if self.key is None:
             raise Exception('API key is empty')
@@ -27,59 +27,79 @@ class cryptocompareAPI(object):
         json = response.json()
         return json
 
+    def utc_to_datetime(self, dataframe, formatting):
+        if(formatting == '1d'):
+            dataframe = dataframe.apply(lambda x: datetime.utcfromtimestamp(x).strftime('%Y-%m-%d'))
+        if(formatting == '1h'):
+            dataframe = dataframe.apply(lambda x: datetime.utcfromtimestamp(x).strftime('%Y-%m-%d %H:%S'))
+        return dataframe
     # Get a single currency -- currenct pricing
     def get_single_currency_price(self, base, quote_list):
         action = 'price'
         params =  {'fsym': base, 'tsyms': quote_list}
         data = self.request(action, params)
-        return data
-
-    def get_single_currency_history(self, base, quote, period, periods_back):
-        ## Determine the correct period
-        if(period == 'daily'):
-            action = 'v2/histoday'
-        elif(period == 'hour'):
-            action = 'v2/histohour'
-        else:
-            action = 'v2/histominute'
-        params = {'fsym': base, 'tsym': quote, 'limit': periods_back}
-        data = self.request(action, params)
         return data['Data']
 
-    def price_to_dataframe(self, currency, days_back):
-        new_header = currency + "_Open"
-        data = json_normalize(self.get_raw_price_data(currency, days_back))
-        data['time'] = data['time'].apply(lambda x: datetime.utcfromtimestamp(x).strftime('%Y-%m-%d'))
+    def get_single_currency_history(self, base, period, periods_back, quote='USD'):
+        action = ''
+        if(period == '1d'):
+            action = 'v2/histoday'
+        elif(period == '1h'):
+            action = 'v2/histohour'
+        elif(period == '1m'):
+            action = 'v2/histominute'
+        else:
+            print("Invalid interval")
+
+        params = {'fsym': base, 'tsym': quote, 'limit': periods_back}
+        data = self.request(action, params)
+        return data['Data']['Data']
+
+    ## This function creates a dataframe from the get_single_currency_price function that only contains a formatted date
+    ## and corresponding price
+    def helper_open_price_to_dataframe(self, base, period, periods_back,quote='USD'):
+        new_header = base + "_open"
+        data = json_normalize(self.get_single_currency_history(base, period, periods_back))
+
+        if(period == '1d'):
+            data['time'] = self.utc_to_datetime(data['time'], "1d")
+        else:
+            data['time'] = self.utc_to_datetime(data['time'], "1h")
+
         data[new_header] = data['open']
         return data[['time', new_header]]
 
-    def get_multiple_currencies(self, currency_list, days_back, data=None):
+    # Note that this uses the opening price of any day put into the function
+    def get_multiple_currencies(self, base_list, period, periods_back,quote='USD',include_returns=None):
         dataframe_final = pd.DataFrame()
-        for item in currency_list:
-            new_header = item + "_Open"
-            if(item == currency_list[0]):
-                dataframe_final = self.get_open_history(item, days_back)
+
+        ## Loop for creating the DataFrame
+        for currency in base_list:
+            new_header = currency + "_open"
+            if(currency == base_list[0]):
+                dataframe_final = self.helper_open_price_to_dataframe(currency, period, periods_back)
             else:
-                dataframe_final[new_header] = self.get_open_history(item, days_back)[new_header]
-            dataframe_final[item + "_return"] = dataframe_final[new_header].pct_change()
-
-            if(data == 'returns'):
-                dataframe_final.drop([new_header], axis=1, inplace=True)
-
-        dataframe_final.set_index(['time'], inplace=True)
+                dataframe_final[new_header] = self.helper_open_price_to_dataframe(currency, period, periods_back)[new_header]
+            ## Keep inside loop -- add column for generating a pct return next to each price movement. Helpful for calculating indicies. Use include_returns header to activate
+            if(include_returns is not None):
+                dataframe_final[currency + "_return"] = dataframe_final[new_header].pct_change()
         return dataframe_final
 
-    def get_exchange_data(self, exchange_list, days):
+    def get_exchange_data(self, exchange_list, periods_back, quote='USD'):
         dataframe_final = pd.DataFrame()
-        for item in exchange_list:
-            action = "exchange/histoday?tsym=USD&limit={}&e={}".format(days, item)
-            new_header = str(item)
-            if(item == exchange_list[0]):
-                dataframe_final = json_normalize(self.request(action)["Data"])
+        action = "exchange/histoday"
+
+        for exchange in exchange_list:
+            new_header = str(exchange)
+            params = {'tsym': quote, 'limit': periods_back, 'e': exchange}
+
+            if(exchange == exchange_list[0]):
+                dataframe_final = json_normalize(self.request(action, params)["Data"])
                 dataframe_final.rename(columns={'volume': new_header}, inplace=True)
             else:
-                dataframe_final[new_header] = json_normalize(self.request(action)["Data"])['volume']
-        dataframe_final['time'] = dataframe_final['time'].apply(lambda x: datetime.utcfromtimestamp(x).strftime('%Y-%m-%d'))
+                dataframe_final[new_header] = json_normalize(self.request(action, params)["Data"])['volume']
+
+        dataframe_final['time'] = self.utc_to_datetime(dataframe_final['time'], '1d')
         return dataframe_final
 
     def index_data(self, weights, price_data):
@@ -91,6 +111,16 @@ class cryptocompareAPI(object):
         df['index_returns'] = df.sum(axis=1)
         df['index_values'] = (df['index_returns']+1).cumprod()
         return df
+
+    def get_coin_volatility(self, base_list, window, period, periods_back):
+        data = self.get_multiple_currencies(base_list, period, periods_back)
+        for column in data:
+            if(is_numeric_dtype(data[column])):
+                data[column] = data[column].pct_change().rolling(window).std()*(365**0.5)
+        return data
+
+    def get_correlation_data(self, coin_list, start_date, end_date, correlation_periods):
+        return
 
 
 
@@ -104,10 +134,16 @@ class cryptocompareAPI(object):
 if __name__ == '__main__':
     api_key = '60a704b50ae660f09c6e053b1a5fa8027e95d145b3f21061e359d1cb185c5fe4'
     cryptocompare = cryptocompareAPI(api_key)
-    print(cryptocompare.get_single_currency_price('BTC', ['EUR', 'USD']))
-    #exchange = CryptoCompare.get_exchange_data(['Binance', 'Coinbase', 'BitMEX', 'Bitfinex'], 100)
+    print(cryptocompare.get_coin_volatility(['BTC','ETH', 'XRP'], 30, '1d', 365))
 
 
+
+
+
+
+    #exchange = cryptocompare.get_exchange_data(['Binance', 'Coinbase', 'BitMEX', 'Bitfinex'], 100)
+
+    #print(exchange)
     #data = CryptoCompare.get_multiple_currencies(['BTC', 'ETH'], 1000)
     #data = CryptoCompare.get_multiple_currencies(['BTC'], 1200)
     #return_data = CryptoCompare.index_data(august_weights, data)
